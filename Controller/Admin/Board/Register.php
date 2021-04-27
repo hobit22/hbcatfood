1eb2a582eacebee5d62adf37c8636363c04b43de
YONGGYO LEE <yonggyo1981@gmail.com>
2021-04-27 06:00:15 +0000

    강의 소스

---
+++ Controller/Admin/Board/Register.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Controller\Admin\Board;
+
+use App;
+use Controller\Admin\Board\Traits\SubMenu;
+
+/**
+* 게시판 생성 
+*
+*/
+class RegisterController extends \Controller\Admin\Controller
+{
+	use SubMenu;
+	
+	protected $mainCode = "board";
+	private $subCode = "register";
+	
+	public function index()
+	{
+		App::render("Board/form");
+	}
+}
