56d9b73348b819932078615b51c10fcd73384d15
YONGGYO LEE <yonggyo1981@gmail.com>
2021-04-27 05:52:40 +0000

    강의 소스

--- Bootstrap/autoload.php
+++ Bootstrap/autoload.php
@@ -9,7 +9,28 @@ $path = [
 	__DIR__ . "/../Component",
 	__DIR__ . "/../Controller",
 ];
-$fileList = App::includeFiles($path);
+$tmp = App::includeFiles($path);
+$fileList = $tmp2 = [];
+foreach ($tmp as $t) {
+	$pi = pathinfo($t);
+	if ($pi['filename'] == 'Controller') {
+		$fileList[] = $t;
+	} else if (!preg_match("/Component/", $t)) {
+		$dirs = explode("/", $pi['dirname']);
+		$tmp2[count($dirs)][] = $t;
+	} else {
+		$fileList[] = $t;
+	}
+}
+
+krsort($tmp2);
+
+foreach ($tmp2 as $list) {
+	foreach ($list as $f) {
+		$fileList[] = $f;
+	}
+}
+
 foreach ($fileList as $f) {
 	include_once $f;
 }
