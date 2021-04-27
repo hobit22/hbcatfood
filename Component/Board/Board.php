0bf1596703a9aa91a7d604235e2209d52d3442cb
YONGGYO LEE <yonggyo1981@gmail.com>
2021-04-27 05:35:25 +0000

    강의 소스

---
+++ Component/Board/Board.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Component\Board;
+
+/**
+* 게시판 관련 Component
+*
+*/
+class Board
+{
+	
+}
--- Controller/Admin/Board/List.php
+++ Controller/Admin/Board/List.php
@@ -2,12 +2,22 @@
 
 namespace Controller\Admin\Board;
 
+use App;
+use Controller\Admin\Board\Traits\SubMenu;
+
 /**
 * 게시판 목록 
 *
 */
 class ListController extends \Controller\Admin\Controller
 {
+	use SubMenu;
+	
 	protected $mainCode = 'board';
 	private $subCode = "list";
+	
+	public function index()
+	{
+		App::render("Board/list");
+	}
 }
