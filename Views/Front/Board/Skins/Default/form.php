3900bcaa9567c255dfa2e3162a200e77c33b77a8
YONGGYO LEE <yonggyo1981@gmail.com>
2021-04-28 04:13:29 +0000

    강의 소스

---
+++ Views/Front/Board/Skins/Default/form.php
@@ -0,0 +1,22 @@
+<!-- Default 스킨 form.php -->
+<div class='board_skin_default'>
+<dl>
+	<dt>작성자</dt>
+	<dd>
+		
+	</dd>
+</dl>
+<dl>
+	<dt>제목</dt>
+	<dd></dd>
+</dl>
+<dl>
+	<dt>내용</dt>
+	<dd></dd>
+</dl>
+<dl>
+	<dt>이미지</dt>
+	<dd></dd>
+</dl>
+</div>
+<!--// board_skin_default -->
--- Views/Front/Board/form.php
+++ Views/Front/Board/form.php
@@ -1,6 +1,10 @@
+<form id='frmBoard' name='frmBoard' method='post' action='<?=siteUrl("board/indb")?>' target='ifrmHidden' autocomplete='off' enctype='multipart/form-data'>
+	<input type='hidden' name='id' value='<?=$id?>'>
 <?php
 // 게시판 작성,수정 
 $path = __DIR__ . "/Skins/".$boardSkin."/form.php";
 if (file_exists($path)) {
 	include $path;
 }
+?>
+</form>
