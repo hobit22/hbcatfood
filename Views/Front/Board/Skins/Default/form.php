103088c42ddd291da0a8d136504fb9f3ce63f58c
YONGGYO LEE <yonggyo1981@gmail.com>
2021-04-28 04:22:17 +0000

    강의 소스

--- Views/Front/Board/Skins/Default/form.php
+++ Views/Front/Board/Skins/Default/form.php
@@ -3,20 +3,26 @@
 <dl>
 	<dt>작성자</dt>
 	<dd>
-		
+		<input type='text' name='poster' value='<?=isLogin()?$_SESSION['member']['memNm']:""?>'>
 	</dd>
 </dl>
 <dl>
 	<dt>제목</dt>
-	<dd></dd>
+	<dd>
+		<input type='text' name='subject'>
+	</dd>
 </dl>
 <dl>
 	<dt>내용</dt>
-	<dd></dd>
+	<dd>
+		<textarea name='contents' id='contents'></textarea>
+	</dd>
 </dl>
 <dl>
 	<dt>이미지</dt>
 	<dd></dd>
 </dl>
+<button type='button' class='cancel_btn'>취소하기</button>
+<button type='button' class='write_btn'>작성하기</button>
 </div>
 <!--// board_skin_default -->
