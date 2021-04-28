d161acd85ab60e86b2e5465a9fabbedcc0d969ec
YONGGYO LEE <yonggyo1981@gmail.com>
2021-04-28 07:34:59 +0000

    강의 소스

--- Views/Front/Board/form.php
+++ Views/Front/Board/form.php
@@ -1,6 +1,9 @@
 <form id='frmBoard' name='frmBoard' method='post' action='<?=siteUrl("board/indb")?>' target='ifrmHidden' autocomplete='off' enctype='multipart/form-data'>
-	<input type='hidden' name='mode' value='register'>
+	<input type='hidden' name='mode' value='<?=isset($idx)?"update":"register"?>'>
 	<input type='hidden' name='boardId' value='<?=$id?>'>
+	<?php if (isset($idx)) : // 게시글 수정 ?>
+	<input type='hidden' name='idx' value='<?=$idx?>'>
+	<?php endif; ?>
 <?php
 // 게시판 작성,수정 
 $path = __DIR__ . "/Skins/".$boardSkin."/form.php";
