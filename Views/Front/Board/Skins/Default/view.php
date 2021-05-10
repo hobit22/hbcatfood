6ff99de9de0f0fc0503ad27face88cd06cc3d096
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-10 06:37:29 +0000

    ,

--- Views/Front/Board/Skins/Default/view.php
+++ Views/Front/Board/Skins/Default/view.php
@@ -28,7 +28,11 @@
 	
 	<a href='<?=siteUrl("board/list")?>?id=<?=$boardId?>' class='btn1'>글목록</a>
 	<a href='<?=siteUrl("board/write")?>?id=<?=$boardId?>' class='btn1'>글쓰기</a>
+	<?php if ($updatePossible) : ?>
 	<a href='<?=siteUrl("board/update")?>?idx=<?=$idx?>' class='btn1'>글수정</a>
+	<?php endif; ?>
+	<?php if ($deletePossible) : ?>
 	<a href='<?=siteUrl("board/indb")?>?mode=delete&idx=<?=$idx?>' onclick="return confirm('정말 삭제하시겠습니까?');" class='btn1'>글삭제</a>
+	<?php endif; ?>
 </div>
 <!--// board_skin_default -->
