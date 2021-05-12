af59e7d6ff21154dc60b7b1fb7ad066b8916f98b
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-12 03:49:21 +0000

    .

--- Component/Goods/Goods.php
+++ Component/Goods/Goods.php
@@ -35,9 +35,29 @@ class Goods
 	*
 	* @param String $mode 등록(register), 수정(update)
 	* @return $this
+	* @throw GoodsAdminException 
 	*/
 	public function validator($mode = "register")
 	{
+		
+		/** 필수 데이터 체크 */
+		$missing = [];
+		foreach($this->requiredColumns as $column => $colNm) {
+			if (!$this->params[$column]) {
+				$missing[] = $colNm;
+			}
+		}
+		
+		if ($missing) {
+			throw new GoodsAdminException("필수 입력 항목 누락 - ".implode(",", $missing));
+		}
+		
+		/**
+			판매가, 소비자가 데이터 형식(숫자) 체크 
+			숫자가 아니면 0
+		*/
+		if (!is_numeric($this->params['salePrice'])) $this->params['salePrice'] = 0;
+		if (!is_numeric($this->params['consumerPrice'])) $this->params['consumerPrice'] = 0;
 		
 		return $this;
 	}
@@ -49,6 +69,23 @@ class Goods
 	*/
 	public function register()
 	{
-		
+		$inData = $this->getCommonColumns($this->params);
+	}
+	
+	/**
+	* 상품 등록, 수정 공통 컬럼 
+	*
+	* @param Array $params 
+	* @return Array
+	*/
+	public function getCommonColumns($params)
+	{
+		$columns = [
+			'goodsNm' => isset($params['goodsNm'])?$params['goodsNm']:"",
+			'shortDescription' => isset($params['shortDescription'])?$params['shortDescription']:"",
+			'salePrice' => isset($params['salePrice'])?$params['salePrice']:0,
+			'consumerPrice' => isset($params['consumerPrice'])?$params['consumerPrice']:0,
+			'description' => isset($params['description'])?$params['description']:"",
+		];
 	}
 }
