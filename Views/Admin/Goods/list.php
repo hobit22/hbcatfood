b4491dbfe3757725a7542403e82828349079c565
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-12 07:34:08 +0000

    .

--- Views/Admin/Goods/list.php
+++ Views/Admin/Goods/list.php
@@ -4,11 +4,33 @@
 		<table class='table_rows'>
 			<thead>
 				<tr>
-					<th>
+					<th width='20'>
 						<input type='checkbox' class='selectAll' data-target-name='goodsNo'>
 					</th>
+					<th width='80'>상품번호</th>
+					<th colspan='2'>상품</th>
+					<th width='100'>판매가</th>
+					<th width='100'>소비자가</th>
+					<th>관리</th>
 				</tr>
 			</thead>
+			<tbody>
+		<?php foreach ($list as $li) : ?>
+				<tr>
+					<td align='center'>
+						<input type='checkbox' name='goodsNo[]' value='<?=$li['goodsNo']?>'>
+					</td>
+					<td align='center'><?=$li['goodsNo']?></td>
+					<td width='50'>이미지</td>
+					<td width='300'><?=$li['goodsNm']?></td>
+					<td align='center'><?=number_format($li['salePrice'])?>원</td>
+					<td align='center'><?=number_format($li['consumerPrice'])?>원</td>
+					<td>
+						<a href='<?=siteUrl("admin/goods/update")?>?goodsNo=<?=$li['goodsNo']?>' class='btn2'>상품수정</a>
+					</td>
+				</tr>
+		<?php endforeach; ?>
+			</tbody>
 		</table>
 	</form>
 </div>
