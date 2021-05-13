12179f6d0e662ec6bbb69c38b1610efded14544e
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-13 08:06:35 +0000

    .

--- Views/Admin/Goods/_form_option.php
+++ Views/Admin/Goods/_form_option.php
@@ -7,12 +7,39 @@
 	</div>
 	
 	<div class='inner'></div>
-	<span class='btn1 create_opt_items dn'>옵션 항목생성하기</span>
+	<span class='btn1 mt20 create_opt_items dn'>옵션 항목생성하기</span>
 </div>
 <!--// opt_names -->
+
+<div class='opt_items'>
+	
+</div>
+
 
 <script type='text/html' id='opt_name_template'>
 	<span class='opt_name'>
 		<input type='text' name='optNames[]' value='' class='w120'>
 	</span>
 </script> 
+
+<script type='text/html' id='opt_item_template'>
+	<div class='opt_item'>
+		<div class='opt_name_tit'>[[optName]]</div>
+		<div class='btns'>
+			<i class='xi-plus-square-o add'></i>
+			<i class='xi-minus-square-o remove'></i>
+		</div>
+		<table class='table_rows'>
+			<thead>
+				<tr>
+					<th>옵션항목</th>
+					<th>옵션가</th>
+					<th>재고</th>
+					<th>품절여부</th>
+					<th>진열</th>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>
+	</div>
+</script>
