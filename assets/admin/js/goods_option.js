8bc50ba11c3aab85259862f782c092b63098b6d4
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-13 07:58:36 +0000

    .

--- assets/admin/js/goods_option.js
+++ assets/admin/js/goods_option.js
@@ -10,6 +10,8 @@ const goodsOption = {
 	addOptName : function() {
 		const html = $("#opt_name_template").html();
 		$(".opt_names > .inner").append(html);
+		
+		$(".create_opt_items").removeClass("dn"); // 항목 생성 버튼 노출 
 	},
 	/**
 	* 옵션명 삭제 
@@ -17,6 +19,11 @@ const goodsOption = {
 	*/
 	removeOptName : function() {
 		$(".opt_names .opt_name:last-child").remove();
+		
+		$list = $(".opt_names .opt_name");
+		if ($list.length == 0) {
+			$(".create_opt_items").removeClass('dn').addClass('dn');
+		}
 	}
 }
 
