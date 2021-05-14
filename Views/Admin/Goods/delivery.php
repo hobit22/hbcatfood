904bf3560aa1739308244d50d713b2f56bb4305d
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-14 07:24:51 +0000

    .

--- Views/Admin/Goods/delivery.php
+++ Views/Admin/Goods/delivery.php
@@ -5,9 +5,21 @@
 		<table class='table_cols'>
 			<tr>
 				<th>설정이름</th>
-				<td></td>
+				<td>
+					<input type='text' name='deliveryName'>
+				</td>
+				<th>배송비</th>
+				<td>
+					<input type='text' name='deliveryPrice'>
+				</td>
+				<th>합배송여부</th>
+				<td>
+					<select name='isTogether'>
+						<option value='1'>같은 설정간 합배송</option>
+						<option value='0'>개별배송비 부과</option>
+					</select>
+				</td>
 			</tr>
-			
 		</table>
 	</form>
 </div>
