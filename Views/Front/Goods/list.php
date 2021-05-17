4690491b9b8147c06bb38dd18bae41d1a90bbde0
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-17 08:59:40 +0000

    .

--- Views/Front/Goods/list.php
+++ Views/Front/Goods/list.php
@@ -1,17 +1,17 @@
 <ul class='goods_list'>
-<?php foreach ($list as $li) : ?>
+<?php foreach ($list as $li) :  ?>
 	<li class='goods'>
 		<a href='<?=siteUrl("goods/view")?>?goodsNo=<?=$li['goodsNo']?>'>
 			<div class='images'>
 			<?php if (isset($li['images']['list'][0])) : ?>
-				<img src='<?=$li['images']['list'][0]?>'>
+			<img src='<?=$li['images']['list'][0]['url']?>'>
 			<?php endif; ?>
 			</div>
 			<div class='goods_nm'>
 				<?=$li['goodsNm']?>
 			</div>
 			<div class='short_desc'>
-				<?=$li['short_desc']?>
+				<?=$li['shortDescription']?>
 			</div>
 			<div class='price_wrap'>
 				<?php if ($li['consumerPrice']) : // 소비자가 ?>
