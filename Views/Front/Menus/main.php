c08c76dbacbd0c3da16fc1a5f5ea64c187405096
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-17 07:47:12 +0000

    .

--- Views/Front/Menus/main.php
+++ Views/Front/Menus/main.php
@@ -1,5 +1,17 @@
+<!-- 프론트 상단 메뉴 -->
+<div class='main_top'>
+	<?php if (isLogin()) : // 회원 ?>
+		<?=$_SESSION['member']['memNm']?>(<?=$_SESSION['member']['memId']?>)님 로그인... 
+		<a href='<?=siteUrl("member/update")?>'>회원정보수정</a>
+		<a href='<?=siteUrl("member/logout")?>'>로그아웃</a>
+	<?php else : // 비회원 ?>
+		<a href='<?=siteUrl("member/login")?>'>로그인</a>
+		<a href='<?=siteUrl("member/join")?>'>회원가입</a>
+	<?php endif; ?>
+	<a href='<?=siteUrl("order/cart")?>'>장바구니</a>
+</div>
 <!-- 프론트 로고 영역 -->
-<div class='logo'>
+<div class='main_logo'>
 	<a href='<?=siteUrl("main/index")?>'>
 		<img src='<?=siteUrl("assets/front/images/logo.png")?>'>
 	</a>
