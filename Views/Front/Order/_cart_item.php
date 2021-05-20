e773647307a6f929dd01614f068f72b219328fd3
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-20 08:12:12 +0000

    .

--- Views/Front/Order/_cart_item.php
+++ Views/Front/Order/_cart_item.php
@@ -1,21 +1,29 @@
 		<table class='table_rows cart_goods'>
 			<thead>
 				<tr>
+					<?php if (!isset($isOrder)) : ?>
 					<th width='20'>
 						<input type='checkbox' class='selectAll' data-target-name='cartNo' checked>
 					</th>
+					<?php endif; ?>
 					<th colspan='2'>상품</th>
 					<th width='150'>구매수량</th>
 					<th width='100'>합계</th>
+					<?php if (!isset($isOrder)) : ?>
 					<th width='130'></th>
+					<?php endif; ?>
 				</tr>
 			</thead>
 			<tbody>
 		<?php foreach ($list as $li) : ?>
 			<tr>
+				<?php if (!isset($isOrder)) : // 장바구니 ?>
 				<td align='center'>
 					<input type='checkbox' name='cartNo[]' value='<?=$li['cartNo']?>' checked>
 				</td>
+				<?php else : // 주문서 ?>
+					<input type='hidden' name='cartNo[]' value='<?=$li['cartNo']?>'>
+				<?php endif; ?>
 				<td width='80'>
 					<?php if ($li['goodsImage']) : ?>
 					<a href='<?=siteUrl("goods/view?goodsNo={$li['goodsNo']}")?>' target='_blank'> 
@@ -39,10 +47,12 @@
 				<td align='center'>
 					<span class='goodsTotal' data-basic='<?=($li['salePrice'] + $li['addPrice'])?>'><?=number_format($li['totalGoodsPrice'])?></span>원
 				</td>
+				<?php if (!isset($isOrder)) : ?>
 				<td align='center'>
 					<a class='btn2 delete' href='<?=siteUrl("order/indb?mode=delete&cartNo[]={$li['cartNo']}")?>' target='ifrmHidden' onclick="return confirm('정말 삭제하시겠습니까?');">상품삭제</a><br>
 					<span class='btn2 order'>바로구매</span>
 				</td>
+				<?php endif; ?>
 			</tr>
 		<?php endforeach; ?>
 			</tbody>
