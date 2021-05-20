9d4e2555acbe3806ca42359625f7af2a5105d42d
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-20 02:21:44 +0000

    강의 소스

--- Controller/Front/Order/Cart.php
+++ Controller/Front/Order/Cart.php
@@ -13,7 +13,7 @@ class CartController extends \Controller\Front\Controller
 	public function index()
 	{
 		$cart = App::load(\Component\Order\Cart::class);
-		$list = $cart->getGoods(); // 장바구니 상품
-		App::render("Order/cart");
+		$data = $cart->getGoods(); // 장바구니 상품
+		App::render("Order/cart", $data);
 	}
 }
---
+++ Views/Front/Order/cart.php
@@ -0,0 +1,13 @@
+<div class='cart_page'>
+	<table class='table_rows cart_goods'>
+		<thead>
+			<tr>
+				<th width='20'>
+					<input type='checkbox' class='selectAll' data-target-name='cartNo'>
+				</th>
+				<th colspan='2'>상품</th>
+			</tr>
+		</thead>
+	</table>
+</div>
+<!--// cart_page -->
