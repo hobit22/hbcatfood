0faa15b5cf368ea621472675feba6ce9ac028a6e
YONGGYO LEE <yonggyo1981@gmail.com>
2021-05-20 02:34:06 +0000

    .

--- Views/Front/Order/cart.php
+++ Views/Front/Order/cart.php
@@ -1,13 +1,19 @@
 <div class='cart_page'>
-	<table class='table_rows cart_goods'>
-		<thead>
-			<tr>
-				<th width='20'>
-					<input type='checkbox' class='selectAll' data-target-name='cartNo'>
-				</th>
-				<th colspan='2'>상품</th>
-			</tr>
-		</thead>
-	</table>
+	<form method='post' action='<?=siteUrl("order/indb")?>' target='ifrmHidden' aucomplete='off'>
+		<input type='hidden' name='mode' value='order'>
+		<table class='table_rows cart_goods'>
+			<thead>
+				<tr>
+					<th width='20'>
+						<input type='checkbox' class='selectAll' data-target-name='cartNo'>
+					</th>
+					<th colspan='2'>상품</th>
+					<th width='150'>구매수량</th>
+					<th width='100'>합계</th>
+					<th width='100'></th>
+				</tr>
+			</thead>
+		</table>
+	</form>
 </div>
 <!--// cart_page -->
